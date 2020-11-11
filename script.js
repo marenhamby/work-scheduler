e3acaa27d42054d1e9e4410c53b8a8227d76479f
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-11 16:29:22 -0500

    adjusted formatting to be more clean, adjusted one error in the last comment to be more descriptive of what the function does, removed commented code piece that was no longer necessary

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // make sure the page has fully loaded before doing anything
-$(document).ready(function() {
+$(document).ready(function () {
 
     //add the current date and time information at the top of the page
     $("#currentDay").text(moment().format("dddd, Do MMMM YYYY, h:mm a"))
@@ -18,53 +18,52 @@ $(document).ready(function() {
 
 
     //add function for the save buttons to save the information for that row to local storage
-    $(".saveBtn").on("click", function(){
+    $(".saveBtn").on("click", function () {
 
-      //add variable to pull the value of the task column from the row where the button was pressed so it can later be saved to local storage.
-      var tasks = $(this).siblings(".description").val();
+        //add variable to pull the value of the task column from the row where the button was pressed so it can later be saved to local storage.
+        var tasks = $(this).siblings(".description").val();
 
-      //add variable to pull the id of the row where the button was pressed so it can be saved with the value to local storage-- instead of setting static text as the key, then it's more specifically descriptive
-      var time = $(this).parent().attr("id");
+        //add variable to pull the id of the row where the button was pressed so it can be saved with the value to local storage-- instead of setting static text as the key, then it's more specifically descriptive
+        var time = $(this).parent().attr("id");
 
-      //save the tasks to local storage with the key for the line it's from
-      localStorage.setItem(time, tasks)
+        //save the tasks to local storage with the key for the line it's from
+        localStorage.setItem(time, tasks)
     })
 
 
     //add function to figure out the current time
     var currentTime = parseInt(moment().format("H"))
-    // console.log(parseInt(moment().format("H")))
+
 
     //add function to compare current time to the times on the chart and change the color format--already listed as classes in the css--according to what is current, past, and future
     function changeColor() {
 
-      $(".schedule-row").each(function(){
+        $(".schedule-row").each(function () {
 
-        //add variable for each row to have a time quantity that can be compared to the current time
-        var rowTime = parseInt($(this).attr("id"))
+            //add variable for each row to have a time quantity that can be compared to the current time
+            var rowTime = parseInt($(this).attr("id"))
 
-        //set if statements to change the color of the description of a row depending on if it is before, during, or after the present hour of the user
-        if (rowTime < currentTime) {
-          console.log("before time");
-          $(this).children(".description").addClass("past")
-        }
+            //set if statements to change the color of the description of a row depending on if it is before, during, or after the present hour of the user
+            if (rowTime < currentTime) {
+                console.log("before time");
+                $(this).children(".description").addClass("past")
+            }
 
-        if(rowTime === currentTime) {
-          console.log("now");
-          $(this).children(".description").addClass("present")
-        }
+            if (rowTime === currentTime) {
+                console.log("now");
+                $(this).children(".description").addClass("present")
+            }
 
-        if (rowTime > currentTime) {
-          console.log("the future");
-          $(this).children(".description").addClass("future")
-        }
+            if (rowTime > currentTime) {
+                console.log("the future");
+                $(this).children(".description").addClass("future")
+            }
 
-      })
+        })
 
     }
 
-    //run function about changing time
+    //run function about changing colors of the schedule rows
     changeColor()
 
-
-  })
+})
